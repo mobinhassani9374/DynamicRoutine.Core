aefd618d74464e4b22c7f81aa96189e3b5b88e0d
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-03-17 22:31:55 +0330

    add header and footer

---
+++ DynamicRoutine.Core/wwwroot/assets/js/script.js
@@ -0,0 +1,27 @@
+$(function() {
+    $('.header__hide-sidebar').on('click',function(){
+        if($('body').hasClass('hide-sidebar')) {
+            $('body').removeClass('hide-sidebar');
+            $('.menu').css('margin-right','0px')
+            $('.main').css('margin-right','200px')
+        }
+        else {
+            $('body').addClass('hide-sidebar');
+            $('.menu').css('margin-right','-200px')
+            $('.main').css('margin-right','0px')
+        }
+    })
+
+    $(window).resize(function(){
+        $('.main').removeAttr('style')
+        $('.menu').removeAttr('style')        
+        if (window.innerWidth < 992) {  
+            $('body').addClass('page-small');
+        }
+        else {
+            $('body').removeClass('page-small');
+        }        
+    })
+
+    $(window).resize();
+})
