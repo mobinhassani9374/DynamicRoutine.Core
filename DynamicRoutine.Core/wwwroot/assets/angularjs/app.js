8f23b497493c3fc1377dbaddeb79a26e6781f807
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-03-17 16:54:29 +0330

    add file angular and app

---
+++ DynamicRoutine.Core/wwwroot/assets/angularjs/app.js
@@ -0,0 +1,39 @@
+﻿var app = angular.module('myApp', ['chieffancypants.loadingBar', 'toaster', 'hSweetAlert', 'ngFileUpload', 'ADM-dateTimePicker'])
+.config(function (cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = true;
+}).controller('loadingBar', function ($scope, $http, $timeout, cfpLoadingBar) {
+
+    var getRandomSubreddit = function () {
+        var sub = $scope.subreddits[Math.floor(Math.random() * $scope.subreddits.length)];
+
+        // ensure we get a new subreddit each time.
+        if (sub == $scope.subreddit) {
+            return getRandomSubreddit();
+        }
+
+        return sub;
+    };
+
+    $scope.fetch = function () {
+        $scope.subreddit = getRandomSubreddit();
+        $http.jsonp('http://www.reddit.com/r/' + $scope.subreddit + '.json?limit=50&jsonp=JSON_CALLBACK').success(function (data) {
+            $scope.posts = data.data.children;
+        });
+    };
+
+    $scope.start = function () {
+        cfpLoadingBar.start();
+    };
+
+    $scope.complete = function () {
+        cfpLoadingBar.complete();
+    }
+    // fake the initial load so first time users can see it right away:
+    $scope.start();
+    $scope.fakeIntro = true;
+    $timeout(function () {
+        $scope.complete();
+        $scope.fakeIntro = false;
+    }, 750);
+
+});
