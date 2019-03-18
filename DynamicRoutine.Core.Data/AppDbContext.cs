1d4b1b15fa5f3c56b06fd8af12a29aa00a3f8c25
mobinhassani <mobinhassani9374@gmail.com>
2019-03-18 12:18:22 +0330

    add AppDbContext and UserMapper

---
+++ DynamicRoutine.Core.Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DynamicRoutine.Core.Data.Mapper;
+using DynamicRoutine.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DynamicRoutine.Core.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new UserMapper());
+        }
+    }
+}
---
+++ DynamicRoutine.Core.Data/Mapper/UserMapper.cs
@@ -0,0 +1,22 @@
+﻿using DynamicRoutine.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicRoutine.Core.Data.Mapper
+{
+    public class UserMapper : BaseMapper<User, int>
+    {
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(c => c.UserName).IsRequired().HasMaxLength(100);
+
+            builder.Property(c => c.Password).IsRequired().HasMaxLength(100);
+
+            builder.Property(c => c.FullName).HasMaxLength(200);
+        }
+    }
+}
