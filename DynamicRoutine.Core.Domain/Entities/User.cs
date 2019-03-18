cd856ec9fd6929c6bcf4d65fca70515f3dfbc82c
mobinhassani <mobinhassani9374@gmail.com>
2019-03-18 12:08:24 +0330

    add user and Base Entity

---
+++ DynamicRoutine.Core.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace DynamicRoutine.Core.Domain.Entities
+{
+    public class User : BaseEntity<int>
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string FullName { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
