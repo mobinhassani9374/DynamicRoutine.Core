d113e7736c3dca09ecaa57209dccdc2f291b4621
mobinhassani <mobinhassani9374@gmail.com>
2019-03-18 14:44:07 +0330

    develop of Login

--- DynamicRoutine.Core/Controllers/AccountController.cs
+++ DynamicRoutine.Core/Controllers/AccountController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DynamicRoutine.Core.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DynamicRoutine.Core.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserRepository _userRepository;
+
+        public AccountController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public IActionResult Login()
         {
             return View();
