47036f14a4fff589c88cbdabe8c86b846f17e170
mobinhassani <mobinhassani9374@gmail.com>
2019-03-18 12:34:46 +0330

    add BaseRepository

---
+++ DynamicRoutine.Core.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicRoutine.Core.Data.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
