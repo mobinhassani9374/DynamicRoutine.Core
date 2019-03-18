1982128b17ab2c1b88f12fd3e71e685f6800fa3f
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-03-18 11:21:11 +0330

    modify isotope

--- DynamicRoutine.Core/wwwroot/assets/js/script.js
+++ DynamicRoutine.Core/wwwroot/assets/js/script.js
@@ -6,16 +6,16 @@ $(function() {
         if($('body').hasClass('hide-sidebar')) {
             $('body').removeClass('hide-sidebar');
             $('.menu').css('margin-right','0px')
-            $('.main').css('margin-right','200px')
+            $('.main').css('margin-right','200px')        
         }
         else {
             $('body').addClass('hide-sidebar');
             $('.menu').css('margin-right','-200px');
-            $('.main').css('margin-right','0px');
-        }
+            $('.main').css('margin-right','0px');         
+        }        
     })
 
-    $(window).resize(function(){
+    $(window).resize(function() {
         $('.main').removeAttr('style')
         $('.menu').removeAttr('style')        
         if (window.innerWidth < 992) {  
@@ -23,17 +23,19 @@ $(function() {
         }
         else {
             $('body').removeClass('page-small');
-        }        
+        }             
     })
 
     $(window).resize();
 
-    
-    // isotope
+    isotopeFunc()
+})
 
+
+function isotopeFunc() {
     $('.grid').isotope({
         // options
         itemSelector: '.grid-item', 
         isOriginLeft: false,               
     });
-})
+}
