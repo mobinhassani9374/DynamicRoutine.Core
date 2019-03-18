aa2c5b1138e1fa029adb76087830920cb1bc9c7d
mobinhassani <mobinhassani9374@gmail.com>
2019-03-18 12:50:19 +0330

    add UserRepository and develop of BaseRepository

--- DynamicRoutine.Core.Data/Repositories/BaseRepository.cs
+++ DynamicRoutine.Core.Data/Repositories/BaseRepository.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿using DynamicRoutine.Core.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using DynamicRoutine.Core.Utility;
+using System.Linq.Expressions;
 
 namespace DynamicRoutine.Core.Data.Repositories
 {
-    public abstract class BaseRepository
+    public abstract class BaseRepository<TEntity, Tkey> where TEntity : BaseEntity<Tkey>
     {
         protected readonly AppDbContext _context;
 
         public BaseRepository(AppDbContext context)
         {
             _context = context;
+        }
+
+        public IQueryable<TEntity> AsQueryable()
+        {
+            return _context.Set<TEntity>().AsQueryable();
+        }
+
+        public ServiceResult Create(TEntity entity)
+        {
+            _context.Add(entity);
+            return _context.Save();
+        }
+
+        public ServiceResult Update(TEntity entity)
+        {
+            _context.Update(entity);
+            return _context.Save();
         }
     }
 }
